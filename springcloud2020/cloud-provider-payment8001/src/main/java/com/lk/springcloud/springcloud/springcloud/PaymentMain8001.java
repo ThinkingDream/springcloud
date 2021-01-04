99d3d44087042d090ad82e8f6fec3614877c9e00
liukun <1152828343@qq.com>
2021-01-04 11:22:35 +0800

    add modules

---
+++ springcloud2020/cloud-provider-payment8001/src/main/java/com/lk/springcloud/springcloud/springcloud/PaymentMain8001.java
@@ -0,0 +1,16 @@
+package com.lk.springcloud.springcloud.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author liukun
+ * @date 2020/12/1 下午10:43
+ */
+@SpringBootApplication
+public class PaymentMain8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8001.class);
+
+    }
+}
---
+++ springcloud2020/cloud-provider-payment8001/src/main/java/com/lk/springcloud/springcloud/springcloud/service/PaymentService.java
@@ -0,0 +1,13 @@
+package com.lk.springcloud.springcloud.springcloud.service;
+
+import com.lk.springcloud.springcloud.springcloud.entities.Payment;
+
+/**
+ * @author liukun
+ * @date 2020/12/1 下午11:20
+ */
+public interface PaymentService {
+    public int create(Payment payment);
+
+    public Payment getPaymentById(Long id);
+}
